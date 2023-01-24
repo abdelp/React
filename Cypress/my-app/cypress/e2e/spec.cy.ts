28b165fb02db7d3e9bfea812ee7ab0176c06da46
Abdel P <aperez4@godaddy.com>
2023-01-24 16:36:21 -0300

    Finish first E2E test

--- Cypress/my-app/cypress/e2e/spec.cy.ts
+++ Cypress/my-app/cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    expect(true).to.equal(false)
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
+    cy.get('.action-email').type('fake@email.com')
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
