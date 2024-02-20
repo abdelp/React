bd7b0e7022d9f4fbaea4dd99c27dcab02a1544da
Abdel P <aperez4@godaddy.com>
2024-02-20 17:23:45 -0300

    Add dep-pre-bundling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import VisibilityFilters from './components/VisibilityFilters';
 import './App.css';
+import {useIntl, FormattedDate} from 'react-intl'
 
 const App = () => {
+  const intl = useIntl();
+
   return (
     <div className='todo-app'>
-      <h1>Todo List</h1>
+      <h1>{intl.formatMessage('Todo List')}</h1>
       <AddTodo />
       <TodoList />
       <VisibilityFilters />
