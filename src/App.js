5972a5c040b8c10be7044dd57d4c42638cb5ad28
Abdel P <aperez4@godaddy.com>
2024-06-06 11:42:28 -0400

    Add vite app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import VisibilityFilters from './components/VisibilityFilters';
 import './App.css';
-import {useIntl, FormattedDate} from 'react-intl'
+import { FormattedDate} from 'react-intl'
+import { memo, useState } from 'react';
+
+const Numbers = memo(({ nums, addRandom }) => {
+  console.log('Numbers rendered');
+
+  return (
+    <div>
+      <ul>
+        {nums.map((num, i) => {
+          <li key={i}>{num}</li>
+        })}
+      </ul>
+      <button onClick={addRandom}>Add random</button>
+    </div>
+  )
+});
 
 const App = () => {
-  const intl = useIntl();
+  const [nums, setNums] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const increaseCounter = () => {
+    setCount(count + 1)
+  }
+
+  const addRandom = () => {
+    let randNum = parseInt(Math.random() * 1000, 10);
+    setNums([...nums, randNum]);
+  }
 
   return (
     <div className='todo-app'>
-      <h1>{intl.formatMessage('Todo List')}</h1>
+      <div>
+        Count: {count} &nsbp;
+        <button onClick={increaseCounter}>+</button>
+      </div>
+      <hr />
+      <Numbers nums={nums} addRandom={addRandom} />
       <AddTodo />
       <TodoList />
       <VisibilityFilters />
