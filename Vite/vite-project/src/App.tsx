a31940175191472082721b056e6d85693f7fd48b
Abdel P <aperez4@godaddy.com>
2025-01-07 10:47:35 -0300

    Add axios

--- Vite/vite-project/src/App.tsx
+++ Vite/vite-project/src/App.tsx
@@ -9,6 +9,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import axios from "axios";
 
 const queryClient = new QueryClient();
 
@@ -16,10 +17,10 @@ function Example() {
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: ["repoData"],
     queryFn: async () => {
-      const response = await fetch(
+      const response = await axios.get(
         "https://api.github.com/repos/TanStack/query"
       );
-      return await response.json();
+      return await response.data;
     },
   });
 
