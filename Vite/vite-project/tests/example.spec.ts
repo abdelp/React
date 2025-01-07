2e2f24d16525630801dbf0e8944431eefc121153
Abdel P <aperez4@godaddy.com>
2025-01-07 11:34:11 -0300

    Add test with waitForResponse

--- Vite/vite-project/tests/example.spec.ts
+++ Vite/vite-project/tests/example.spec.ts
@@ -1,20 +1,44 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('navigation', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-  })
+test.describe("navigation", () => {
+  test("mocked text is visible", async ({ page }) => {
+    await page.route(
+      "https://api.github.com/repos/TanStack/query",
+      async (route) => {
+        const originalResponse = await route.fetch();
 
-  test('has title', async ({ page }) => {
+        await route.fulfill({
+          status: originalResponse.status(),
+          headers: originalResponse.headers(),
+          contentType: "application/json", // Ensure content type matches
+          body: JSON.stringify({ description: "Hello Mr. Potatoe" }),
+        });
+      }
+    );
+
+    await page.goto("http://localhost:5173");
+
+    const responsePromise = page.waitForResponse(
+      (response) =>
+        response
+          .url()
+          .includes("https://api.github.com/repos/TanStack/query") &&
+        response.status() === 200
+    );
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    const text = await page.getByText("Hello Mr. Potatoe");
+
+    await responsePromise;
+    await expect(text).toBeVisible();
   });
-  
-  test('get started link', async ({ page }) => {  
+
+  test.skip("get started link", async ({ page }) => {
     // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
-  
+    await page.getByRole("link", { name: "Get started" }).click();
+
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Installation" })
+    ).toBeVisible();
   });
-})
+});
