ddd3ecc7cfee3a6d0fc846db6dc03ce8993f3c15
Abdel Pérez <abdel.perezpy@gmail.com>
2025-02-17 10:32:27 -0300

    Finish adding shared options

--- Vite/vite-mfe/vite.config.ts
+++ Vite/vite-mfe/vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, createLogger } from "vite";
 import react from "@vitejs/plugin-react-swc";
 // import tailwindcss from "@tailwindcss/vite";
+
+const logger = createLogger();
+const loggerWarn = logger.warn;
+
+logger.warn = (msg, options) => {
+  if (msg.includes("vite:css") && msg.includes(" is empty")) return;
+  loggerWarn(msg, options);
+};
 
 export default defineConfig({
   root: ".",
@@ -14,14 +22,31 @@ export default defineConfig({
   cacheDir: "node_modules/.vite",
   resolve: {
     alias: {
-      "@": "/src",
+      "@": "/src", // needs to be an absolute path?
     },
+    dedupe: ["react", "react-dom"],
+    conditions: ["module", "browser", "development|production"],
+  },
+  css: {
+    devSourcemap: true,
+    transformer: "postcss",
+  },
+  json: {
+    namedExports: true,
+    stringify: "auto",
   },
   build: {
-    lib: {
-      entry: "src/main.tsx", // Your app's entry point
-      formats: ["es"], // Required for single-spa
-      fileName: () => "microfrontend.js",
+    cssCodeSplit: false,
+    emptyOutDir: true,
+    minify: true,
+    rollupOptions: {
+      input: "src/main.tsx",
     },
   },
+  logLevel: "info", // less verbose for production
+  customLogger: logger,
+  clearScreen: true,
+  envDir: ".", // customize depending of environment
+  envPrefix: "VITE_", // expose to client via import.meta.env. Should not be set as ''
+  appType: "spa",
 });
